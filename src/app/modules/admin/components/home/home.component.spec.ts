3ec0faf48a6cdade7646cd779ed131b95c8c8ade
Swarnavo Paul <swarnavo.paul2@mindtree.com>
2020-12-17 09:40:22 +0530

    Some specs in home component

--- src/app/modules/admin/components/home/home.component.spec.ts
+++ src/app/modules/admin/components/home/home.component.spec.ts
@@ -1,8 +1,10 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatMenuModule } from '@angular/material/menu';
 import { RouterTestingModule } from '@angular/router/testing';
-
+import { By } from '@angular/platform-browser';
+import {Location} from '@angular/common';
 import { HomeComponent } from './home.component';
+import { Component } from '@angular/core';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -10,8 +12,10 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [HomeComponent],
-      imports: [RouterTestingModule, MatMenuModule],
+      declarations: [HomeComponent,DummyComponent],       //changes
+      imports: [RouterTestingModule.withRoutes([{
+        path:'analysis',component:DummyComponent          //chnages
+      }]), MatMenuModule],
     }).compileComponents();
   });
 
@@ -24,4 +28,65 @@ describe('HomeComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have a button Icy-Lycious on the navigation bar', ()=>{
+    
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[0].nativeElement;
+    expect(nativeButton.textContent).toBe('Icy-Licious');
+    
+  })
+
+  it('should have a button Add Product on the navigation bar', ()=>{
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[1].nativeElement;
+    expect(nativeButton.textContent).toBe('Add Products');
+  })
+
+  it('should have a button View Product on the navigation bar', ()=>{
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[2].nativeElement;
+    expect(nativeButton.textContent).toBe('View Products');
+  })
+
+  it('should have a button Show Orders on the navigation bar', ()=>{
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[3].nativeElement;
+    expect(nativeButton.textContent).toBe('Show Orders');
+  })
+
+  it('should have a button Dashboard on the navigation bar', ()=>{
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[4].nativeElement;
+    expect(nativeButton.textContent).toBe('Dashboard');
+  })
+
+  it('should have a button Subscribers on the navigation bar', ()=>{
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[5].nativeElement;
+    expect(nativeButton.textContent).toBe('Subscribers');
+  })
+
+
+  //changes
+  it('after pressing Icy-licious button should naviate to the analysis page',async ()=>{
+    const location=TestBed.get(Location);
+    const linkDes=fixture.debugElement
+      .queryAll(By.css('button'));
+    const nativeButton: HTMLButtonElement=linkDes[0].nativeElement;
+    nativeButton.click();
+    fixture.detectChanges();
+    fixture.whenStable().then(()=>{
+      expect(location.path()).toBe('/analysis');
+    })
+  })
 });
+
+@Component({template:''})
+class DummyComponent{}
