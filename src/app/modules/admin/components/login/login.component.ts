f19f509566c01684e90ef28b6daaa692c44926fb
Ayush Jaiswal <ayush.jaiswal2@mindtree.com>
2020-12-17 22:03:07 +0530

    commited show passwords

--- src/app/modules/admin/components/login/login.component.ts
+++ src/app/modules/admin/components/login/login.component.ts
@@ -1,64 +1,60 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder,Validators} from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoginService } from '../../../../services/login.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-
-  get username(){
+  get username() {
     return this.loginForm.get('username');
   }
 
-  get password(){
+  get password() {
     return this.loginForm.get('password');
   }
 
-  constructor(private fb:FormBuilder,private _loginService:LoginService,private router:Router) { }
+  constructor(
+    private fb: FormBuilder,
+    private _loginService: LoginService,
+    private router: Router
+  ) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 
   loginForm = this.fb.group({
-    username: ['',[Validators.required]],
-    password: ['',[Validators.required]]
-  })
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]],
+  });
 
-  public failed:boolean = false;
-  public wrongPass:boolean = false;
+  public failed: boolean = false;
+  public wrongPass: boolean = false;
 
-
-  login(){
-    console.log(this.loginForm.value)
-    this._loginService.login(this.loginForm.value)
-      .subscribe(
-        response =>{
-          if(response.msg === "invalid"){
-            this.loginForm.reset();
-            this.failed = true;
-          }
-          else if(response.msg === "failed"){
-            this.loginForm.patchValue({email: this.loginForm.get('username').value,password:''});
-            this.wrongPass = true;
-          }
-          else if(response.msg === "success"){
-            console.log(response); 
-            localStorage.setItem('logged','true');
-            localStorage.setItem('token',response.token)
-            this.router.navigate(['dashboard/analysis'])
-          }
-      })
-    }
-  onChange(){
+  login() {
+    console.log(this.loginForm.value);
+    this._loginService.login(this.loginForm.value).subscribe((response) => {
+      if (response.msg === 'invalid') {
+        this.loginForm.reset();
+        this.failed = true;
+      } else if (response.msg === 'failed') {
+        this.loginForm.patchValue({
+          email: this.loginForm.get('username').value,
+          password: '',
+        });
+        this.wrongPass = true;
+      } else if (response.msg === 'success') {
+        // console.log(response);
+        localStorage.setItem('logged', 'true');
+        localStorage.setItem('token', response.token);
+        this.router.navigate(['dashboard/analysis']);
+      }
+    });
+  }
+  onChange() {
     this.failed = false;
     this.wrongPass = false;
   }
-
-  
-
 }
