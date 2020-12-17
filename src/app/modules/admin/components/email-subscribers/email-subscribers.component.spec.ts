17f5f4c55432c3efe6d76c20ce43d007c19e6b9a
Ayush Jaiswal <ayush.jaiswal2@mindtree.com>
2020-12-17 14:39:02 +0530

    email spec fix

--- src/app/modules/admin/components/email-subscribers/email-subscribers.component.spec.ts
+++ src/app/modules/admin/components/email-subscribers/email-subscribers.component.spec.ts
@@ -1,6 +1,7 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { By } from '@angular/platform-browser';
 
 import { EmailSubscribersComponent } from './email-subscribers.component';
 
@@ -21,7 +22,20 @@ describe('EmailSubscribersComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  describe('Simple Html', () => {
+    it('Should have exports in h2 tag', () => {
+      const linkDes = fixture.debugElement.queryAll(By.css('h2'));
+      const nativeH2: HTMLHeadElement = linkDes[0].nativeElement;
+      expect(nativeH2.textContent).toBe('Export');
+    });
+    it('Should have only one table for Email Subscribers', () => {
+      const tables = fixture.debugElement.queryAll(By.css('table'));
+      expect(tables.length).toBe(1);
+    });
+    it('Table Should have Heading as # and Email ID', () => {
+      const linkDes = fixture.debugElement.queryAll(By.css('table'));
+      const nativeTable: HTMLTableHeaderCellElement = linkDes[0].nativeElement;
+      expect(nativeTable.textContent).toBe('#Email ID');
+    });
   });
 });
