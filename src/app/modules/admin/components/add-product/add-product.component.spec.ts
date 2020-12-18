7ea171e6c560893e593a72d9ae8318d26b8e1480
Rashmi Kumari <rashmi.kumari3@mindtree.com>
2020-12-18 16:06:45 +0530

    added test in add product

--- src/app/modules/admin/components/add-product/add-product.component.spec.ts
+++ src/app/modules/admin/components/add-product/add-product.component.spec.ts
@@ -5,6 +5,7 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ToastrModule } from 'ngx-toastr';
 import { By } from '@angular/platform-browser';
+import { AddProductService } from 'src/app/services/add-product.service';
 describe('AddProductComponent', () => {
   let component: AddProductComponent;
   let fixture: ComponentFixture<AddProductComponent>;
@@ -14,10 +15,9 @@ describe('AddProductComponent', () => {
       declarations: [AddProductComponent],
       imports: [
         RouterTestingModule,
-        HttpClientTestingModule,
         ToastrModule.forRoot(),
       ],
-      providers:[FormBuilder]
+      providers:[FormBuilder,{ provide: AddProductService, useClass: AddProductServiceStub }]
     }).compileComponents();
   });
 
@@ -27,7 +27,11 @@ describe('AddProductComponent', () => {
     fixture.detectChanges();
   });
 
-  describe("form testing", ()=>{
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe("testing initial state of form", ()=>{
     it("initially form should not be valid",()=>{
       expect(component.createForm.valid).toBeFalsy();
     })
@@ -70,10 +74,60 @@ describe('AddProductComponent', () => {
   })
 
 
+  describe("checking for HTML elements", ()=>{
 
-  // describe("checking that only one form is present", ()=>{
-  //   const forms = fixture.nativeElement.querySelectorAll('form');
-  //   expect(forms.length).toBe(1);
-  // })
+    it("checking for number of forms",()=>{
+      const forms = fixture.nativeElement.querySelectorAll('form');
+      expect(forms.length).toBe(1);
+    })
+
+    it("checking for number of labels and their content",()=>{
+      const labels = fixture.nativeElement.querySelectorAll('label');
+      expect(labels.length).toBe(9);
+      expect(labels[0].textContent).toBe("Product Code");
+      expect(labels[1].textContent).toBe("Product Name");
+      expect(labels[2].textContent).toBe("Type");
+      expect(labels[3].textContent).toBe("Price");
+      expect(labels[4].textContent).toBe("Description");
+      expect(labels[5].textContent).toBe("Start date");
+      expect(labels[6].textContent).toBe("End date");
+      expect(labels[7].textContent).toBe("Product Image");
+      expect(labels[8].textContent).toBe("Upload");
+    })
+
+    it("checking for number of inputs and their types",()=>{
+      const inputs = fixture.nativeElement.querySelectorAll("input");
+      expect(inputs.length).toBe(6);
+      expect(inputs[0].type).toBe("text");
+      expect(inputs[1].type).toBe("text");
+      expect(inputs[2].type).toBe("number");
+      expect(inputs[3].type).toBe("date");
+      expect(inputs[4].type).toBe("date");
+      expect(inputs[5].type).toBe("file");
+    })
+
+    it("checking for number of text area",()=>{
+      const textAreas = fixture.nativeElement.querySelectorAll("textarea");
+      expect(textAreas.length).toBe(1);
+    })
+
+    it("checking for number of select tag",()=>{
+      const selects = fixture.nativeElement.querySelectorAll("select");
+      expect(selects.length).toBe(1);
+    })
+
+    it("checking for number of buttons",()=>{
+      const btns = fixture.nativeElement.querySelectorAll("button");
+      expect(btns.length).toBe(1);
+      expect(btns[0].textContent).toBe(" Create ");
+    })
+    
+  })
 
 });
+class AddProductServiceStub{
+  create(product){
+
+  }
+}
+
