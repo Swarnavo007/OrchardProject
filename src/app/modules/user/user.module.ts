1b0b0024097cbc0e1d01a86c5f223b79e6824534
MOHAMED SHEHAS V <mohamedshehas.v2@mindtree.com>
2020-12-18 12:49:56 +0530

    landing page

--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -4,13 +4,14 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { AdminModule } from '../admin/admin.module';
-
+import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [UserComponent],
   imports: [
     CommonModule,
     UserRoutingModule,
+    MatToolbarModule
   ]
 })
 export class UserModule { }
