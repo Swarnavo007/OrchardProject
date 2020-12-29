3c2a3424c867d31691968c265e2b36921dbf60d1
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-29 15:23:15 +0530

    changes made in updateprofile spec file

--- src/app/modules/user/components/update-profile/update-profile.component.spec.ts
+++ src/app/modules/user/components/update-profile/update-profile.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UpdateProfileComponent } from './update-profile.component';
 
@@ -8,7 +11,12 @@ describe('UpdateProfileComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ UpdateProfileComponent ]
+      declarations: [ UpdateProfileComponent ],
+      imports: [
+         HttpClientTestingModule,
+        RouterTestingModule
+      ],
+      providers: [FormBuilder],
     })
     .compileComponents();
   });
