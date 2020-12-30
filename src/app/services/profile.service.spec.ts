49396de8c0b5ca4ee3a8d37fdd348add9630d979
Navaneetha1603 <navaneethakandlapelli@gmail.com>
2020-12-30 10:19:03 +0530

    added name validations

--- src/app/services/profile.service.spec.ts
+++ src/app/services/profile.service.spec.ts
@@ -1,4 +1,5 @@
-import { HttpClientTestingModule,HttpTestingController } from '@angular/common/http/testing';
+
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
@@ -6,25 +7,67 @@ import { ProfileService } from './profile.service';
 
 describe('ProfileService', () => {
   let service: ProfileService;
-  let httpMock:HttpTestingController;
+  let httpMock:HttpTestingController
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [RouterTestingModule, HttpClientTestingModule],
       providers:[ProfileService]
     });
     service = TestBed.inject(ProfileService);
-    httpMock=TestBed.get(HttpTestingController);
+    httpMock=TestBed.get(HttpTestingController)
   });
-
-  describe('service check for profile',()=>{
-    it("should use GET method",()=>{
-      service.getOrders().subscribe();
-      const req=httpMock.expectOne(service.url);
-      expect(req.request.method).toBe('GET')
-    })
-  })
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+  describe("service check",()=>{
+    it("should contain the get method",()=>{
+      service.getOrders().subscribe();
+      const req=httpMock.expectOne(service.url);
+      expect(req.request.method).toBe('GET');
+    });
+
+    it('should get the orders', () => {
+      const dummyData = 
+        {
+          "status": "Delivered",
+          "_id": "5fe9a62facc30d0017cfab47",
+          "userId": "ayush.jaiswal47.aj@gmail.com",
+          "itemPurchased": [
+              {
+                  "_id": "5fe9a627acc30d0017cfab44",
+                  "productQty": 1,
+                  "productId": "CH10",
+                  "productName": "chocolat",
+                  "productPrice": 1000
+              },
+              {
+                  "_id": "5fe9a627acc30d0017cfab45",
+                  "productQty": 1,
+                  "productId": "NAVA",
+                  "productName": "kjfhxk",
+                  "productPrice": 90
+              },
+              {
+                  "_id": "5fe9a629acc30d0017cfab46",
+                  "productQty": 1,
+                  "productId": "CH12",
+                  "productName": "vanilla",
+                  "productPrice": 56
+              }
+          ],
+          "totalPrice": 1146,
+          "orderDate": "2020-12-28T09:32:31.033Z",
+          "__v": 0
+      }
+      
+      service.getOrders().subscribe(res => {
+        expect(res).toEqual(dummyData);
+      });
+  
+      const request = httpMock.expectOne(`https://sumit-icylicious-sep-20.herokuapp.com/getOrders`);
+      expect(request.request.method).toBe('GET');
+      request.flush(dummyData);
+    });
+  })
 });
