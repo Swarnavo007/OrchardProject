2d51eb66edf9ed7a0be3e5a542abb3e66c7f3d6f
Ayush Jaiswal <ayush.jaiswal2@mindtree.com>
2021-01-04 17:27:50 +0530

    stub for cart component

--- src/app/modules/user/components/cart/cart.component.spec.ts
+++ src/app/modules/user/components/cart/cart.component.spec.ts
@@ -1,11 +1,11 @@
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { CartService } from 'src/app/services/cart.service';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { ToastrModule } from 'ngx-toastr';
 import { By } from '@angular/platform-browser';
-import { Router } from '@angular/router';
 
 import { CartComponent } from './cart.component';
+import { from, of } from 'rxjs';
 
 describe('CartComponent', () => {
   let component: CartComponent;
@@ -14,11 +14,8 @@ describe('CartComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CartComponent],
-      imports: [
-        HttpClientTestingModule,
-        RouterTestingModule,
-        ToastrModule.forRoot(),
-      ],
+      imports: [RouterTestingModule, ToastrModule.forRoot()],
+      providers: [{ provide: CartService, useClass: CartServiceStub }],
     }).compileComponents();
   });
 
@@ -27,47 +24,69 @@ describe('CartComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-   
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-  describe('button clicks',()=>{
-    it ('check the working of the showModel button', ()=>
-  {
-    expect(component.submitButton).toBe(false, 'not clicked');
-    component.showModel();
-    expect(component.submitButton).toBe(true, 'clicked');
-  })
+  describe('button clicks', () => {
+    it('check the working of the showModel button', () => {
+      expect(component.submitButton).toBe(true, 'not clicked');
+      component.showModel();
+      expect(component.submitButton).toBe(false, 'clicked');
+    });
 
-  it ('check the working of the closeModel button', ()=>
-  {
-    expect(component.submitButton).toBe(false, 'not clicked');
-    component.closeModel();
-    expect(component.submitButton).toBe(true, 'clicked');
-  })
+    it('check the working of the closeModel button', () => {
+      expect(component.submitButton).toBe(true, 'not clicked');
+      component.closeModel();
+      expect(component.submitButton).toBe(false, 'clicked');
+    });
 
-
-  it ('check the working of the hideloader button', ()=>
-  {
-    expect(component.submitButton).toBe(false, 'not clicked');
-    component.hideloader();
-    expect(component.submitButton).toBe(true, 'clicked');
-  })
-  // it(`after pressing 'BuyNow' button should naviate to the  Buy now! page`, ()=>{
-  //   const router=TestBed.get(Router);
-  //   spyOn(router,'navigateByUrl');
-  //   const linkDes=fixture.debugElement
-  //     .queryAll(By.css('button'));
-  //   const nativeButton: HTMLButtonElement=linkDes[0].nativeElement;
-  //   nativeButton.click();
-  //   expect(router.navigateByUrl).toHaveBeenCalledWith(router.createUrlTree(['/shop']),{
-  //     skipLocationChange:false, replaceUrl:false, state:undefined
-  //   })
-  // })
-
-  
-  
-  })
-
-
+    it('check the working of the hideloader button', () => {
+      expect(component.submitButton).toBe(true, 'not clicked');
+      component.hideloader();
+      expect(component.submitButton).toBe(false, 'clicked');
+    });
+    it(`check if place oder button is there or not`, () => {
+      const linkDes = fixture.debugElement.queryAll(By.css('button'));
+      const nativeButton: HTMLButtonElement = linkDes[2].nativeElement;
+      expect(nativeButton.textContent).toBe(' Place Order ');
+    });
+  });
 });
+class CartServiceStub {
+  getCartDetail() {
+    return from([
+      {
+        _id: '5ff0151e1759a80017240299',
+        userId: 'ayush.jaiswal47.aj@gmail.com',
+        products: [
+          {
+            productQty: 2,
+            _id: '5ff2a840b1305600179ad9a3',
+            productId: 'VN101',
+            productName: 'Chocolate',
+          },
+        ],
+        __v: 17,
+      },
+    ]);
+  }
+  getImageDetail() {
+    return of([
+      {
+        _id: '5ff2a1f8b1305600179ad9a2',
+        productId: 'VN101',
+        productName: 'Chocolate',
+        productType: 'classic',
+        productPrice: 450,
+        productDescription:
+          'Chocolate is made up of milk and butter.It is very famous icecream as well.',
+        productStartDate: '2021-01-04T00:00:00.000Z',
+        productEndDate: '2021-01-13T00:00:00.000Z',
+        productImage:
+          'http://sumit-icylicious-sep-20.herokuapp.com\\uploads/1609736696131.jpg',
+        __v: 0,
+      },
+    ]);
+  }
+}
