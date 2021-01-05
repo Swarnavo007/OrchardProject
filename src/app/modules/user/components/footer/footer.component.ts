e7fc041f731295b697711a4077c75b135dcf3c42
Swarnavo Paul <swarnavo.paul2@mindtree.com>
2021-01-05 17:28:28 +0530

    email bug fixed

--- src/app/modules/user/components/footer/footer.component.ts
+++ src/app/modules/user/components/footer/footer.component.ts
@@ -25,7 +25,8 @@ export class FooterComponent implements OnInit {
         Validators.required,
         Validators.email,
         Validators.pattern(
-          /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+          
+          /^[a-zA-Z][a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
         ),
       ],
     ],
